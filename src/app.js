c3d48f0fda30851c95eddc045b1a544d9a73238d
SofiaReinaudo/*-345671 <sofiareinaudo@gmail.com>
2023-12-06 21:05:16 -0300

    desafio 8

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { Server } from 'socket.io';
 import { engine } from 'express-handlebars';
+import session from 'express-session';
+import MongoStore from "connect-mongo";
 import 'dotenv/config';
 
 import products from './routers/products.js';
@@ -18,6 +20,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: `${process.env.URI_MONGO_DB}/${process.env.NAME_DB}`,
+        ttl: 3600
+    }),
+    secret: process.env.SECRET_SESSION,
+    resave: false,
+    saveUninitialized: true
+}));
+
 app.engine('handlebars', engine());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'handlebars');
@@ -32,8 +44,8 @@ const httpServer = app.listen(PORT, () => {console.log(`Corriendo en el puerto $
 const io = new Server(httpServer);
 
 io.on('connection', async (socket) => {
-
-    const { payload } = await getProductsService({});
+    const limit = 50;
+    const { payload } = await getProductsService({limit});
     const productos = payload;
 
     socket.emit('productos', payload);
--- src/routers/views.js
+++ src/routers/views.js
@@ -1,31 +1,17 @@
 import { Router } from 'express';
-import { getProductsService } from '../services/products.js';
-import { getCartByIdService } from '../services/carts.js';
-
+import {homeView, cartIdView, productsView, chatView, realTimeProductsView, loginGet, registerGet, registerPost, loginPost, logout} from '../controllers/views.js';
+import { admin, auth } from '../middleware/auth.js';
 const router = Router();
 
-router.get('/', async (req, res) => {
-    const {payload} = await getProductsService({});
-    return res.render('home', { payload, styles: 'styles.css', title: 'Home' });
-});
-
-router.get('/realtimeproducts', (req, res) => {
-    return res.render('realTimeProducts', { title: 'Real Time', styles: 'styles.css' });
-});
-
-router.get('/chat', (req, res) => {
-    return res.render('chat', { styles: 'chat.css', title: 'Chat' });
-});
-
-router.get('/products', async (req, res) => {
-    const result = await getProductsService({ ...req.query });
-    return res.render('products', { title: 'productos', result, styles: 'styles.css' });
-});
-
-router.get('/cart/:cid', async (req, res) => {
-    const { cid } = req.params;
-    const carrito = await getCartByIdService(cid);
-    return res.render('cart', {title: 'carrito', carrito, styles:'styles.css'});
-});
+router.get('/', auth,homeView);
+router.get('/realtimeproducts', [auth, admin],  realTimeProductsView);
+router.get('/chat', auth, chatView);
+router.get('/products', auth, productsView);
+router.get('/cart/:cid', auth, cartIdView);
+router.get('/login', loginGet);
+router.post('/login', loginPost);
+router.get('/register', registerGet);
+router.post('/register', registerPost);
+router.get('/logout', logout);
 
 export default router;
